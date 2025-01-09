04e0b777f35caa0dd1dd67bd16fb44879413b8bc
divyanshucodes20 <amrendrsingh960@gmail.com>
2025-01-09 20:53:08 +0530

    Implement initial dashboard layout, user authentication checks, and Prisma setup; add account and transaction schemas

---
+++ actions/dashboard.js
@@ -0,0 +1,54 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/dist/types/server";
+import { revalidatePath } from "next/cache";
+
+const serializeTransaction=(obj)=>{
+   const serialized={...obj};
+   if(obj.balance){
+    serialized.balance=obj.balance.toNumber();
+   } 
+}
+
+export async function createAccount(data) {
+    try {
+        const {userId}=await auth();
+        if(!userId){
+            throw new Error("Unauthorized");
+        }
+        const user=await db.user.findUnique({
+            where:{clerkUserId:userId},
+        });
+        if(!user){
+            throw new Error("User not found");
+        }
+        const balanceFloat=parseFloat(data.balance);
+        if(isNaN(balanceFloat)){
+            throw new Error("Invalid balance");
+        }
+        const existingAccount=await db.account.findMany({
+            where:{userId:user.id},
+        })
+        const shouldBeDefault=existingAccount.length===0?true:data.isDefault;
+        if(shouldBeDefault){
+            await db.account.updateMany({
+                where:{userId:user.id},
+                data:{isDefault:false},
+            });
+        }
+        const account=await db.account.create({
+            data:{
+                ...data,
+                balance:balanceFloat,
+                isDefault:shouldBeDefault,
+                userId:user.id,
+            }
+        });
+        const serializedAccount=serializeTransaction(account);
+        revalidatePath("/dashboard");  
+        return {success:true,data:serializedAccount};
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
