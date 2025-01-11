c00a4c9176c41c5e958766b214f5f8a732b4e267
divyanshucodes20 <amrendrsingh960@gmail.com>
2025-01-11 13:07:49 +0530

    Add receipt scanner component, email sending functionality, and integrate Arcjet for rate limiting and gemini api for recipt data implement cron job for alerts and recurring transactions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/functions";
+import { checkBudgetAlerts, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client:inngest,
   functions: [
     checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
   ],
 });
