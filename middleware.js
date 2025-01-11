8299cc299bbe9411e3c6e3df9eee7f60a39c914c
divyanshucodes20 <amrendrsingh960@gmail.com>
2025-01-11 23:13:32 +0530

    Add NextResponse to middleware for improved route handling

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
@@ -29,6 +30,7 @@ const {userId}=await auth();
   const {redirectToSignIn}=await auth();
   return redirectToSignIn();
   }
+  return NextResponse.next();
 });
 
 export default createMiddleware(aj,clerk);
